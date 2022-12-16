332d973a0946860c6e4da2cb93a7ec5a989ffab9
AyaMu <ayalamuruch@gmail.com>
2022-12-16 12:34:13 -0500

    add initial constants, variables, cached elements, event listeners and functions for gameboard

--- js/app.js
+++ js/app.js
@@ -1,1 +1,73 @@
-console.log("Padded room check!")
+//! console.log("Padded room check!")
+/*-------------------------------- Constants --------------------------------*/
+const winCombos
+
+
+/*---------------------------- Variables (state) ----------------------------*/
+let artifactBoard, turn, winner, tie
+
+
+/*------------------------ Cached Element References ------------------------*/
+const artifactSlotEls = document.querySelectorAll(".slot")
+const messageEls = document.querySelector(".message")
+const artifactBoardEl = document.querySelector(".artifactBoard")
+const resetButtonEl = document.querySelector(".reset")
+
+/*----------------------------- Event Listeners -----------------------------*/
+artifactBoardEl.addEventListener("click", handleClick)
+resetButtonEl.addEventListener("click", init)
+
+
+/*-------------------------------- Functions --------------------------------*/
+
+// ! try a for loop with (n) to push into new array as in engineering channel answer (thanks Emily!)
+
+init()
+
+function init() {
+  artifactBoard = [
+    null, null, null, null, null, null, null,
+    null, null, null, null, null, null, null,
+    null, null, null, null, null, null, null,
+    null, null, null, null, null, null, null,
+    null, null, null, null, null, null, null,
+    null, null, null, null, null, null, null
+  ]
+  turn = 1 
+  winner = false
+  tie = false
+  render()
+}
+
+function render() {
+  updateBoard()
+  updateMessages()
+}
+
+function updateBoard() {
+  artifactBoard
+}
+
+function updateMessages() {
+
+}
+
+function handleClick(evt) {
+
+}
+
+function placeArtifact(){
+
+}
+
+function tieCheck() {
+
+}
+
+function winnerCheck() {
+
+}
+
+function switchTurn() {
+
+}
